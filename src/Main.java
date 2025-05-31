038bb3e318620d145e05d33ca61a63905f5a9f05
Mateus <mateusserranocastro12@gmail.com>
2025-05-31 16:41:03 -0300

    visualizar, editar, excluir - imoveis

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,8 @@ public class Main {
             Pessoa.cadastrarPessoa();
         }
         MenuCadastro.menuCadastro();
+        Imovel.visualizarImoveis();
+        Imovel.editarImovel();
+        Imovel.excluirImovel();
     }
 } 
